f8ffc1de88177fca87b2d0722a98fc10f9d296b9
enoghforme <gen.4048.1105@gmail.com>
2017-04-18 13:08:08 +0900

    zsh,dein関連を削除

--- mklink.bat
+++ mklink.bat
@@ -1,9 +1,8 @@
 set HOMEPATH="C:\Users\gen"
 SETPATH =C:\Users\gen\vim74-kaoriya-win64
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
-mklink %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
-mklink %HOMEPATH%"\.vimperator" %HOMEPATH%"\dotfiles\.vimperator"
 mklink %HOMEPATH%"\.vimperatorrc" %HOMEPATH%"\dotfiles\.vimperatorrc"
 mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
 mklink %HOMEPATH%"\.bash_profile" %HOMEPATH%"\dotfiles\.bash_profile"
+mklink %HOMEPATH%"\.bash_history" %HOMEPATH%"\dotfiles\.bash_history"
 
