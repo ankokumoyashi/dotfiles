f01e8b5f92fa9904ceb5012767b0d6bb34c3e38e
ankokumoyashi <gen.4048.1105@gmail.com>
2019-07-20 21:06:30 +0900

    全部削除

--- mklink.bat
+++
@@ -1,8 +0,0 @@
-set HOMEPATH="C:\Users\gen"
-SETPATH =C:\Users\gen\vim74-kaoriya-win64
-mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
-mklink %HOMEPATH%"\.vimperatorrc" %HOMEPATH%"\dotfiles\.vimperatorrc"
-mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
-mklink %HOMEPATH%"\.bash_profile" %HOMEPATH%"\dotfiles\.bash_profile"
-mklink %HOMEPATH%"\.bash_history" %HOMEPATH%"\dotfiles\.bash_history"
-
