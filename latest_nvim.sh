f8e24d9a899ad701b61c808f1faf88cc49c09a50
ankokumoyashi <gen.4096.1105@gmail.com>
2022-06-14 17:09:06 +0000

    versionUP neovim

--- latest_nvim.sh
+++ latest_nvim.sh
@@ -1,7 +1,9 @@
 # builds
-sudo yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
+sudo yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip patch gettext
+# python3.10
+pip3 install cmake --upgrade
 git clone https://github.com/neovim/neovim.git
 cd neovim
-git checkout v0.3.2
+git checkout v0.6.1
 make CMAKE_BUILD_TYPE=Release
 sudo make install
