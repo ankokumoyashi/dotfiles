4f24cc82f25efa712d3eeb5f62795ed77c26f930
fj161101 <g.inatomi@jp.fujitsu.com>
2022-07-27 13:44:58 +0900

    ホームディレクトリへの配備を追加

--- latest_nvim.sh
+++ latest_nvim.sh
@@ -7,3 +7,8 @@ cd neovim
 git checkout v0.6.1
 make CMAKE_BUILD_TYPE=Release
 sudo make install
+mkdir ~/.config
+mkdir ~/.config/nvim
+cp ./init.vim ~/.config/nvim
+cp ./dein.toml ~/.config/nvim
+cp ./dein_lazy.toml ~/.config/nvim
