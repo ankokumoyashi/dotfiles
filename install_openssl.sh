72ffa20d7e11e504f66557aa64bdf50d58afd986
ankokumoyashi <gen.4048.1105@gmail.com>
2023-02-18 00:06:33 +0900

    openssl

---
+++ install_openssl.sh
@@ -0,0 +1,9 @@
+yum install -y epel-release
+yum install -y openssl11 openssl11-devel
+
+echo 'export CFLAGS=$(pkg-config --cflags openssl11)' >> /etc/bashrc
+echo 'export LDFLAGS=$(pkg-config --libs openssl11)' >> /etc/bashrc
+
+./configure
+make
+sudo make install
