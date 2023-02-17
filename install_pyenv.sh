eb17000b64565e6aeb33ab6b8fb6bca2f6042bee
ankokumoyashi <gen.4048.1105@gmail.com>
2023-02-18 00:07:53 +0900

    pyenv

---
+++ install_pyenv.sh
@@ -0,0 +1,7 @@
+yum install -y git gcc zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel libuuid-devel xz-devel libffi-devel 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> /etc/bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> /etc/bashrc
+echo 'eval "$(pyenv init --path)"' >> /etc/bashrc
+source ~/.bash_profile
+source ~/.bashrc
