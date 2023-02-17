bc44fc4bd28da43824b79781c13bacdda2964785
ankokumoyashi <gen.4048.1105@gmail.com>
2023-02-18 00:08:39 +0900

    fix openssl install script

--- install_openssl.sh
+++ install_openssl.sh
@@ -3,7 +3,3 @@ yum install -y openssl11 openssl11-devel
 
 echo 'export CFLAGS=$(pkg-config --cflags openssl11)' >> /etc/bashrc
 echo 'export LDFLAGS=$(pkg-config --libs openssl11)' >> /etc/bashrc
-
-./configure
-make
-sudo make install
